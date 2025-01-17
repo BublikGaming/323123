dc82a71951f0c1ecc8b7967d8d0b3cf4d860f1ae
BublikGaming <mgmerree@gmail.com>
2025-01-17 13:36:39 +0300

    first commit

---
+++ abc.js
@@ -0,0 +1,13 @@
+/#1/
+// Создаем переменную str и присваиваем ей значение 'abcde'
+let str = 'abcde';
+
+// Выводим символы 'a', 'b' и 'e'
+console.log(str[0]); // 'a'
+console.log(str[1]); // 'b'
+console.log(str[4]); // 'e'
+/#2/
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const secondsInHour = secondsInMinute * minutesInHour;
+console.log(`Количество секунд в часе: ${secondsInHour}`);
